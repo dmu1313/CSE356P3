e7a6139b2634187bddf71af99c2a7b0bb58a4566
dmu1313 <dmu1313@gmail.com>
2019-03-25 05:10:09 -0400

    Add missing build react folder

---
+++ react/frontend/build/precache-manifest.192389d22108140201a954c18a546e47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2d12c1512cb5c4c90dd1",
+    "url": "/static/js/main.002c7898.chunk.js"
+  },
+  {
+    "revision": "596f49930ee7d5d2ee08",
+    "url": "/static/js/2.8027013b.chunk.js"
+  },
+  {
+    "revision": "2d12c1512cb5c4c90dd1",
+    "url": "/static/css/main.c34f9102.chunk.css"
+  },
+  {
+    "revision": "8f09658bacc3206cde06edcf23d06a4e",
+    "url": "/index.html"
+  }
+];

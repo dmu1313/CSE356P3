f73ee67a705bc998314aa54551dc4b5a04cd95d5
dmu1313 <dmu1313@gmail.com>
2019-04-09 04:41:35 -0400

    Fixed nginx config file.

--- server/ElasticUtils.js
+++ server/ElasticUtils.js
@@ -3,7 +3,8 @@
 // Must customize
 
 const { Client } = require('@elastic/elasticsearch');
-const client = new Client({ node: 'http://localhost:9200' });
+// const client = new Client({ node: 'http://localhost:9200' });
+const client = new Client({node: 'http://192.168.122.16:9200'}); // My ElasticSearch IP for now.
 
 module.exports = {
     getElasticClient: function() {

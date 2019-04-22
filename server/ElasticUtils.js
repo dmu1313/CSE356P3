cad16d97a5ad794b6abd03d1b24069c45afa466b
dmu1313 <dmu1313@gmail.com>
2019-04-22 10:56:48 -0400

    config

--- server/ElasticUtils.js
+++ server/ElasticUtils.js
@@ -4,7 +4,10 @@
 
 const { Client } = require('@elastic/elasticsearch');
 // const client = new Client({ node: 'http://localhost:9200' });
-const client = new Client({node: 'http://192.168.122.16:9200'}); // My ElasticSearch IP for now.
+// const client = new Client({node: 'http://192.168.122.16:9200'}); // My ElasticSearch IP for now.
+const client = new Client({
+    hosts: ['http://192.168.122.16:9200', 'http://192.168.122.xx:9200', 'http://192.168.122.xx:9200']
+});
 
 module.exports = {
     getElasticClient: function() {

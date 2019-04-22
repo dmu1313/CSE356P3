5cca27e45e4c2ac8604fb2b236b3769053599b7c
dmu1313 <dmu1313@gmail.com>
2019-04-22 07:48:13 -0400

    changes

--- server/MemcachedUtils.js
+++ server/MemcachedUtils.js
@@ -1,6 +1,6 @@
 
 var Memcached = require('memcached');
-var memcached = new Memcached('192.168.122.20:11211');
+var memcached = new Memcached('192.168.122.23:11211');
 
 module.exports = {
     memcached: memcached

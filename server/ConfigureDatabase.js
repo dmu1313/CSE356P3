96f155e33ac0a82f574c551bc5fa65d90af0c4b3
dmu1313 <dmu1313@gmail.com>
2019-05-14 19:42:12 -0400

    Fix

--- server/ConfigureDatabase.js
+++ server/ConfigureDatabase.js
@@ -21,17 +21,17 @@ const COLLECTION_QUESTION_UPVOTE = constants.COLLECTION_QUESTION_UPVOTE;
 
 
 // Sharding commands:
-sh.enableSharding("Final")
-sh.shardCollection("Final.Cookies", {val: "hashed"})
-sh.shardCollection("Final.Users", {userId: "hashed"})
-sh.shardCollection("Final.Questions", {questionId: "hashed"})
-sh.shardCollection("Final.Answers", {answerId: "hashed"})
-sh.shardCollection("Final.Ip_Views", {questionId: "hashed"})
-sh.shardCollection("Final.User_Views", {questionId: "hashed"})
-sh.shardCollection("Final.A_upvote", {aid: "hashed"})
-sh.shardCollection("Final.Q_upvote", {qid: "hashed"})
-sh.shardCollection("Final.MEDIA", {_id: "hashed"})
-sh.shardCollection("Final.Media_U", {_id: "hashed"})
+// sh.enableSharding("Final")
+// sh.shardCollection("Final.Cookies", {val: "hashed"})
+// sh.shardCollection("Final.Users", {userId: "hashed"})
+// sh.shardCollection("Final.Questions", {questionId: "hashed"})
+// sh.shardCollection("Final.Answers", {answerId: "hashed"})
+// sh.shardCollection("Final.Ip_Views", {questionId: "hashed"})
+// sh.shardCollection("Final.User_Views", {questionId: "hashed"})
+// sh.shardCollection("Final.A_upvote", {aid: "hashed"})
+// sh.shardCollection("Final.Q_upvote", {qid: "hashed"})
+// sh.shardCollection("Final.MEDIA", {_id: "hashed"})
+// sh.shardCollection("Final.Media_U", {_id: "hashed"})
 
 
 // Already Implemented:

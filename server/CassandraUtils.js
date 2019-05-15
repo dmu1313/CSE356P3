cb9bc18e60ebf406f7e0a71a2524d59b5c54084b
dmu1313 <dmu1313@gmail.com>
2019-05-15 00:42:13 -0400

    More migrations to upcloud

--- server/CassandraUtils.js
+++ server/CassandraUtils.js
@@ -8,7 +8,8 @@ var cassandraTable = "imgs";
 var cassandraFullName = cassandraKeyspace + "." + cassandraTable;
 
 const client = new cassandra.Client({
-    contactPoints: ['192.168.122.37', '192.168.122.31'],
+    contactPoints: ['10.3.7.107', '10.3.7.108', '10.3.7.110'],
+    // contactPoints: ['192.168.122.37', '192.168.122.31'],
     localDataCenter: 'test_dc'
     // keyspace: cassandraKeyspace
 });
